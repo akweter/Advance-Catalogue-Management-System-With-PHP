cc95d00309a39f73ce535ce05a68179b93f10ae1
Angel Dev Team <71665600+john-BAPTIS@users.noreply.github.com>
2023-04-11 02:10:34 +0000

    gitignore file added to ptoject

--- client/index.php
+++ client/index.php
@@ -121,6 +121,7 @@
         <meta name="generator" content="Angel Dev Team">
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
         <title>#1 Online Market | Persol</title>
+        <link rel="stylesheet" href="../../node_modules/bootstrap.min.css">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <style>
